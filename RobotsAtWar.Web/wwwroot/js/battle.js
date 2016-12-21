fc86d10e4ce581faadc1c6dad2cd6f6022bea2bc
Tomas Baškys <tomas.baskys@adform.com>
2016-12-21 13:59:49 +0200

    Battle start countdown timer.

--- RobotsAtWar.Web/wwwroot/js/battle.js
+++ RobotsAtWar.Web/wwwroot/js/battle.js
@@ -6,6 +6,8 @@ function onPageLoad() {
     enableButtonPopovers(".action_buttons .rest", ".rest_buttons");
 
     actionInpactTextElement = $(".action_impact_text")[0];
+
+    startPreparationCountdown(3);
 }
 
 function enableButtonPopovers(popoverSelector, contentSelector) {
@@ -100,7 +102,7 @@ function clearActionBar(timeToWait) {
     var actionBar = $('.action_progress_bar .progress-bar');
 
     setTimeout(function () {
-        actionBar.css("transition", "unset ");
+        actionBar.css("transition", "unset");
 
         actionBar.css("width", "0");
     }, timeToWait * 1000 + 100);
@@ -122,7 +124,7 @@ function setDefenceText(defenceState) {
 
     switch (defenceState) {
         case "start":
-            actionInpactTextElement.innerHTML = "Defending";
+            actionInpactTextElement.innerHTML = "&#9960";
             break;
         case "finish":
             actionInpactTextElement.innerHTML = "";
@@ -130,4 +132,20 @@ function setDefenceText(defenceState) {
         default:
             actionInpactTextElement.innerHTML = "";
     }
+}
+
+function startPreparationCountdown(time) {
+    var timerElement = $(".timer h1")[0];
+
+    var countdownId = setInterval(function () {
+        timerElement.innerHTML = --time;
+        if (time === 0) {
+            timerElement.innerHTML = "START!";
+        }
+    }, 1000);
+
+    setTimeout(function () {
+        clearInterval(countdownId);
+        timerElement.innerHTML = "";
+    }, (time+1) * 1000);
 }
