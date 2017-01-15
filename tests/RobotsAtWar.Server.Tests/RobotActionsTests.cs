8e0e7535d3ab90ce3c0d30d31ed83bdd053294a3
Tomas Baškys <tomas.baskys@adform.com>
2017-01-15 14:03:09 +0200

    Moved projects under src folder.

---
+++ tests/RobotsAtWar.Server.Tests/RobotActionsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace RobotsAtWar.Server.Test
+{
+    [TestFixture]
+    public class RobotActionsTests
+    {
+        private static Robot _robot;
+        private static Robot _enemy;
+
+        [SetUp]
+        public void Setup()
+        {
+            _robot = new Robot();
+            _enemy = new Robot();
+
+            _robot.Enemy = _enemy;
+            _enemy.Enemy = _robot;
+        }
+    }
+}
